a817592dfe5fb71380f5cfa8c31e08aa8c3bf5cd
Yatao Li <yatao.li@live.com>
2016-08-29 14:28:45 +0800

    Update cluster-deploy.sh

--- cluster-deploy.sh
+++ cluster-deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 user=azureuser
+port=1234
 
 # Looping through a file: 
 # http://stackoverflow.com/questions/1521462/looping-through-the-content-of-a-file-in-bash
 while read server
 do 
-    scp -P 2223 ssr-init.sh $user@$server:~/; 
+    scp -P $port ssr-init.sh $user@$server:~/; 
     # -t ensures remote sudo will work: 
     # http://stackoverflow.com/questions/10310299/proper-way-to-sudo-over-ssh
-    ssh -t -p 2223 $user@$server "chmod +x ssr-init.sh && ./ssr-init.sh"; 
+    ssh -t -p $port $user@$server "chmod +x ssr-init.sh && ./ssr-init.sh"; 
 done < servers.txt
